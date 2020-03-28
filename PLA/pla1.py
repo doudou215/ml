d76d34e0307d637820ae6b4d6e060ff1026b52b5
Zhang YongLiang <dream20151224@163.com>
2020-03-28 18:51:43 +0800

    pocket version

--- PLA/pla1.py
+++ PLA/pla1.py
@@ -11,29 +11,35 @@ def loadData(filename):
     y = data[:, col:col+1]
     return X, y
 
-def pla1(X, y, w):
+def pla1(X, y, w, n):
     num = 0
     prepos = 0
     while(True):
         ypred = np.sign(np.dot(X, w.T))
         ypred[np.where(ypred == 0)] = -1#np.where() return the indexs which satisfy the condition
-        index = np.where(ypred != y)[0]
+        index = np.where(ypred != y)[0]#return row of where ypred!= y
         if not index.any():
             break
         if not index[index >= prepos].any():
-            prepos = 0
+            prepos = 0#return the valuegg
         pos = index[index >= prepos][0]
         prepos = pos
-        w += y[pos]*X[pos, :]
+        w +=n*y[pos]*X[pos, :]
         num += 1
     return num, w
 
 
 X, y = loadData('data.txt')
-w = np.zeros((1, X.shape[1]))
-ret, w = pla1(X, y, w)
-print(w )
-print(ret)
+ret = 0
+for i in range(2000):
+    row = np.random.permutation(X.shape[0])
+    Xp = X[row, :]
+    yp = y[row, :]
+    w = np.zeros((1, X.shape[1]))
+    num, w = pla1(Xp, yp, w,0.5)
+    ret += num
+print(w)
+print(ret/2000)
 
 
 
